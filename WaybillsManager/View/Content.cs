cd2e9d19b7045ba0ff16d257c452274d3a953031
Frindel <m_storogev@mail.ru>
2023-02-12 13:41:26 +0300

    Добавьте файлы проекта.

---
+++ WaybillsManager/View/Content.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WaybillsManager.View
+{
+	internal abstract class Content
+	{
+		public void Close()
+		{
+			
+		}
+	}
+}
